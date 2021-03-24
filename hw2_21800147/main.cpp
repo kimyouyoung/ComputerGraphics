15c9ea822838afcbfceb09ec76d8de29161616e4
kimyouyoung <>
2021-03-24 16:09:10 +0900

    updated

--- hw2_21800147/main.cpp
+++ hw2_21800147/main.cpp
@@ -8,8 +8,8 @@
 
 typedef std::vector<GLfloat> GLvec;
 
-GLuint VertexArrays[2];
-GLuint Buffers[2][2];
+GLuint VertexArrays[3];
+GLuint Buffers[3][2];
 GLuint active_vao = 0; //index for the active vertex array object
 GLvec vertices[2]; // 2d vertex list
 GLvec colors[3]; // vertex color (r,g,b) list
@@ -22,6 +22,8 @@ void get_circle_2d(GLvec& p, int sectors, GLfloat radius);
 void get_rect_2d(GLvec& p, GLfloat width, GLfloat height);
 void get_vertex_color(GLvec& color, GLuint n, GLfloat r, GLfloat g, GLfloat b);
 void bind_buffer(GLuint buffer, GLvec& vec, int program,
+	const GLchar* attri_name, GLint attri_size);
+void share_buffer(GLuint buffer, int program,
 	const GLchar* attri_name, GLint attri_size);
 
 void main(int argc, char** argv)
@@ -37,6 +39,7 @@ void main(int argc, char** argv)
 	}
 	init();
 	glutDisplayFunc(display);
+	// keyboard callback function in GLUT
 	glutKeyboardFunc(keyboard);
 	glutMainLoop();
 }
@@ -44,8 +47,10 @@ void main(int argc, char** argv)
 void keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
-	case '1': active_vao = 0; glutPostRedisplay(); break;
-	case '2': active_vao = 1; glutPostRedisplay(); break;
+		// Let the GLUT redraw the screen by calling display. (the correct number for keyboard.)
+		case '1': active_vao = 0; glutPostRedisplay(); break;
+		case '2': active_vao = 1; glutPostRedisplay(); break;
+		case '3': active_vao = 2; glutPostRedisplay(); break;
 	}
 }
 
@@ -53,8 +58,12 @@ void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBindVertexArray(VertexArrays[active_vao]);
-	glDrawArrays(active_vao == 0 ? GL_TRIANGLE_FAN : GL_TRIANGLES,
-		0, vertices[active_vao].size() / 2);
+	// if active_vao == 1, make rectangle
+	if(active_vao == 1)
+		glDrawArrays(GL_TRIANGLES, 0, vertices[active_vao].size() / 2);
+	// else, make circle (same vertices - share)
+	else
+		glDrawArrays(GL_TRIANGLE_FAN, 0, vertices[0].size() / 2);
 	glFlush();
 }
 
@@ -65,16 +74,23 @@ void init()
 	get_rect_2d(vertices[1], 1.0f, 0.8f);
 	get_vertex_color(colors[0], vertices[0].size() / 2, 0.8f, 0.2f, 0.5f);
 	get_vertex_color(colors[1], vertices[1].size() / 2, 0.9f, 0.9f, 0.1f);
+	// add blue color
+	get_vertex_color(colors[2], vertices[0].size() / 2, 0.3f, 0.3f, 1.0f);
 
-	glGenVertexArrays(2, VertexArrays);
+	glGenVertexArrays(3, VertexArrays);
 	for (int i = 0; i < 2; ++i) {
 		glBindVertexArray(VertexArrays[i]);
 		glGenBuffers(2, Buffers[i]);
 		bind_buffer(Buffers[i][0], vertices[i], program, "vPosition", 2);
 		bind_buffer(Buffers[i][1], colors[i], program, "vColor", 3);
 	}
+
     // share buffer
+	glBindVertexArray(VertexArrays[2]);
+	share_buffer(Buffers[0][0], program, "vPosition", 2);
     // generate color buffer
+	glGenBuffers(2, Buffers[2]);
+	bind_buffer(Buffers[2][1], colors[2], program, "vColor", 3);
 }
 
 int build_program()
@@ -142,8 +158,18 @@ void bind_buffer(GLuint buffer, GLvec& vec, int program,
 	const GLchar* attri_name, GLint attri_size)
 {
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	// Put the data in the actual buffer.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vec.size(),
 		vec.data(), GL_STATIC_DRAW);
+	GLint location = glGetAttribLocation(program, attri_name);
+	glVertexAttribPointer(location, attri_size, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(location);
+}
+
+void share_buffer(GLuint buffer, int program,
+	const GLchar* attri_name, GLint attri_size)
+{
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	GLint location = glGetAttribLocation(program, attri_name);
 	glVertexAttribPointer(location, attri_size, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(location);
